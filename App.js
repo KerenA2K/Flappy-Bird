9a5b54bae1c8f89fb0d16d9b186b035b59f18e15
unknown <2020102753@dhvsu.edu.ph>
2024-05-03 18:11:58 +0800

    penguin

--- App.js
+++ App.js
@@ -2,39 +2,52 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Canvas, useImage, Image } from "@shopify/react-native-skia";
 import { useWindowDimensions } from 'react-native';
-import {useSharedValue, withTiming, Easing, withSequence, withRepeat} from 'react-native-reanimated';
+import {useSharedValue, withTiming, Easing, withSequence, withRepeat, useFrameCallback} from 'react-native-reanimated';
 import { useEffect } from 'react';
+
+const gravity = 300;
 
 export default function App() {
   const {width, height} = useWindowDimensions();
-  const bg = useImage(require('./assets/sprites/background-day.png'));
-  const bird = useImage(require('./assets/sprites/redbird-upflap.png'));
-  const pipe = useImage(require('./assets/sprites/pipe-green.png'));
-  const pipeTop = useImage(require('./assets/sprites/pipe-green-top.png'));
-  const floor = useImage(require('./assets/sprites/base.png'));
+  const bg = useImage(require('./assets/sprites/bg.png'));
+  const penguin = useImage(require('./assets/sprites/penguin-mid.png'));
+  const pipe = useImage(require('./assets/sprites/pipe-bottom.png'));
+  const pipeTop = useImage(require('./assets/sprites/pipe-top.png'));
+  const floor = useImage(require('./assets/sprites/floor.png'));
 
-  const x = useSharedValue(width - 50)
+  const x = useSharedValue(width - 10);
+  const penguinYpos = useSharedValue(height/2);
+  const penguinVelocity = useSharedValue(100);
+  
+   useFrameCallback(({timeSincePreviousFrame: dt})=>{
+     if (!dt){
+       return;
+     }
+     penguinYpos.value = penguinYpos.value + (penguinVelocity.value * dt)/1000;
+     penguinVelocity.value = penguinVelocity.value + (gravity * dt)/1000;
 
-  useEffect (() => {
-    x.value = 
-    withRepeat ( 
-      withSequence (
-        withTiming(-150, {duration:3000, easing: Easing.linear}),
-        withTiming(width, {duration: 0})
-      ), -1
-    )
-  }, [])
+   });
+
+   useEffect (() => {
+     x.value = 
+     withRepeat ( 
+       withSequence (
+         withTiming(-200, {duration:3000, easing: Easing.linear}),
+         withTiming(width, {duration: 0})
+       ), -1
+     )
+   }, [])
 
 
   const pipeOffset = 0;
   return (
     <View style={styles.container}>
-      <Canvas style={{ width, height }}>
+      <Canvas style={{ width, height }} onTouch={() => (penguinVelocity.value = -200)}>
 
         <Image 
           image={bg} 
           width={width} 
-          height={height} 
+          height={height- 70} 
           fit={'cover'}
         />
 
@@ -64,10 +77,10 @@ export default function App() {
         />
 
         <Image 
-          image={bird}
-          width={42}
-          height={34}
-          y={height / 2}
+          image={penguin}
+          width={95}
+          height={95}
+          y={penguinYpos}
           x={width / 4}
           fit={'contain'}
         />
